2939745c94e930103847ccc2b7f75e6d47ff0467
LadyPi <ms.vanessa.farias.dm@gmail.com>
2016-05-11 15:08:20 -0700

    geoquakes exercise

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,28 @@
 // define globals
+alert("hi");
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
 var template;
 
 $(document).on("ready", function() {
+ 	$quakesList = $.ajax( {
+	method:"GET",
+	url: weekly_quakes_endpoint
+})
+ 	.done (function(data) {
+ 		// console.log(data);
 
+ 		var earthquake = data.features;
 
+ 		var source = $("#quakes-template").html();
+ 		// console.log("lat" + data.features[0].geometry.coordinates[0]);
+ 	
+ 		template = Handlebars.compile(source);
+ 		
+ 		var quakesTemplate = template({quakes: earthquake});
+
+ 		$("#info").append(quakesTemplate);
+ });
 });
+
